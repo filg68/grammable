7bccb4cbdc62ce7c3196aa92a20657fae762413d
Fil <runfilrun@gmail.com>
2019-04-09 19:09:40 +0000

    Added message validation to model and updated gram_controller to return appropriate error.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -12,7 +12,7 @@ class GramsController < ApplicationController
     if @gram.valid?
       redirect_to root_path
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
