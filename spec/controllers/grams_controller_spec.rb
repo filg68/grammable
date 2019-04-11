db528ef9aa8764dd0619c537d98079dcd74033f7
Fil <runfilrun@gmail.com>
2019-04-11 20:09:47 +0000

    Added tests to make sure logged in user created gram before edit, destroy or update actions.

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
   describe "gram#destroy action" do
+    it "shouldn't let a user who did not create the gram destroy a gram" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      delete :destroy, params: { id: gram.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+
     it "shouldn't let unauthenticated users destroy a gram" do
       gram = FactoryBot.create(:gram)
       delete :destroy, params: {id: gram.id }
@@ -26,6 +34,14 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#update action" do
+    it "shouldn't let users who didn't create the gram update it" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      patch :update, params: { id: gram.id, gram: { message: 'wahoo' } }
+      expect(response).to have_http_status(:forbidden)
+    end
+
     it "shouldn't let unauthenticated users update a gram" do
       gram = FactoryBot.create(:gram)
       patch :update, params: {id: gram.id, gram: {message: "Hello!"}}
@@ -61,6 +77,14 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#edit action" do
+    it "shouldn't let users who didn't create the gram update it" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :edit, params: { id: gram.id, gram: { message: 'wahoo' } }
+      expect(response).to have_http_status(:forbidden)
+    end
+
     it "shouldn't let unauthenticated users edit a gram" do
       gram = FactoryBot.create(:gram)
       get :edit, params: { id: gram.id }
@@ -137,7 +161,4 @@ RSpec.describe GramsController, type: :controller do
       expect(Gram.count).to eq gram_count
     end
   end
-
-
-
 end
